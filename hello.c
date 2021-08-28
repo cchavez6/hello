e402f0328c72d0652a380bfd4d31f6fdaea35770
Caden Chavez <cchavez6@huskers.unl.edu>
2021-08-28 02:42:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+    * Author: Caden Chavez
+    * Date: 8/27/21
+*/
+#include <stdlib.h>
+#include <stdio.h>
+   int main(int argc, char **argv) {
+
+     printf("Hello World!\n");
+
+return 0; }
